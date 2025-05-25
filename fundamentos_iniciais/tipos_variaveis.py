e5a27cca440a2968cae75c2eb6f4fb55c2671b91
Michele <michele.nascf@gmail.com>
2025-05-25 15:05:38 -0300

    Adiciona f-string.

--- fundamentos_iniciais/tipos_variaveis.py
+++ fundamentos_iniciais/tipos_variaveis.py
@@ -6,3 +6,4 @@ mulher = True
 print(type(idade), type(peso), type(nome), type(mulher)) # exibe tipos de variáveis
 print("A " + str(nome) + " tem " + str(idade) + " anos e pesa " + str(peso) + "kg.") # concatena variáveis
 print("A {} tem {} anos e pesa {}kg." .format(nome, idade, peso)) # utiliza método format()
+print(f"A {nome} tem {idade} anos e pesa {peso}kg.") #utiliza f-string
