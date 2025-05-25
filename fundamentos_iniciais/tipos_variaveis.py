a3e5daabcc318540efaf839c4e1055d39963cd1d
Michele <michele.nascf@gmail.com>
2025-05-25 15:12:07 -0300

    Muda tipo de variável.

--- fundamentos_iniciais/tipos_variaveis.py
+++ fundamentos_iniciais/tipos_variaveis.py
@@ -6,4 +6,12 @@ mulher = True
 print(type(idade), type(peso), type(nome), type(mulher)) # exibe tipos de variáveis
 print("A " + str(nome) + " tem " + str(idade) + " anos e pesa " + str(peso) + "kg.") # concatena variáveis
 print("A {} tem {} anos e pesa {}kg." .format(nome, idade, peso)) # utiliza método format()
-print(f"A {nome} tem {idade} anos e pesa {peso}kg.") #utiliza f-string
+print(f"A {nome} tem {idade} anos e pesa {peso}kg.") # utiliza f-string
+
+# mudança de tipo de variável
+faturamento = float(input("Qual é o seu faturamento?"))
+custo = float(input("Qual é o seu custo?"))
+
+lucro = faturamento - custo
+
+print(lucro)
