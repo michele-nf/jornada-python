cf0ce2285275a76c6dc340829660d9174ac46dc0
Michele <michele.nascf@gmail.com>
2025-05-25 13:03:03 -0300

    Adiciona concatenação de vários tipos.

--- fundamentos_iniciais/tipos_variaveis.py
+++ fundamentos_iniciais/tipos_variaveis.py
@@ -4,3 +4,4 @@ nome = "Michele Ferreira"
 mulher = True
 
 print(type(idade), type(peso), type(nome), type(mulher))
+print("A " + str(nome) + " tem " + str(idade) + " anos e pesa " + str(peso) + "kg.")
