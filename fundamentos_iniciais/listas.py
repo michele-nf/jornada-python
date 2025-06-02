ffc6ce1211754dcc40722fc4d5559a10ec8c1647
Michele <michele.nascf@gmail.com>
2025-06-02 18:55:09 -0300

    Adiciona índices em lista, consulta e modificação de valores.

---
+++ fundamentos_iniciais/listas.py
@@ -0,0 +1,10 @@
+produtos = ["tv", "celular", "mouse", "teclado", "tablet"]
+vendas = [1000, 1500, 350, 270, 900]
+
+vendas[1] = 2000
+print("Vendas do produto {} foram de {} unidades".format(produtos[1], vendas[1]))
+print(vendas)
+
+texto = "michele@teste.com"
+texto = texto.replace("michele", "isabela")
+print(texto)
