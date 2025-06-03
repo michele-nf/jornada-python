0c041e49c509291b1e48db90bb999a18f5b155b0
Michele <michele.nascf@gmail.com>
2025-06-03 20:16:56 -0300

    Adiciona funções para adicionar e remover itens da lista, e duas formas de tratar erros.

--- fundamentos_iniciais/listas.py
+++ fundamentos_iniciais/listas.py
@@ -8,3 +8,29 @@ print(vendas)
 texto = "michele@teste.com"
 texto = texto.replace("michele", "isabela")
 print(texto)
+
+i = produtos.index("teclado")
+qtd_vendas = vendas[i] 
+
+print(f"A quantidade de vendas do produto {produtos[i]} é de: {qtd_vendas} unidades.") 
+
+# adicionar item
+produtos.append("geladeira")
+print(produtos)
+
+# remover item
+produtos.pop(0)
+produtos.remove("geladeira")
+print(produtos)
+
+# 2 formas de tratar erro
+remover_produtos = "casa"
+if remover_produtos in produtos:
+    produtos.remove(remover_produtos)
+else:
+    print(f"O produto {remover_produtos} não existe na lista.")
+
+try:
+    produtos.remove(remover_produtos)
+except:
+    print(f"O produto {remover_produtos} não existe na lista.")
