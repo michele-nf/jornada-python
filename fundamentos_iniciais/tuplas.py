4778a05ad4c9d837f8694d4ef2e20f7cd59549a9
Michele <michele.nascf@gmail.com>
2025-06-20 20:45:36 -0300

    Adiciona tuplas.

---
+++ fundamentos_iniciais/tuplas.py
@@ -0,0 +1,11 @@
+vendas = ('Michele', 32, 1500.50, 'Vendedora', True)
+nome = vendas[0]
+print(f'Nome: {nome}')
+idade = vendas[1]
+print(f'Idade: {idade}')
+vendas_totais = vendas[2]
+print(f'Vendas Totais: R$ {vendas_totais}')    
+cargo = vendas[3]
+print(f'Cargo: {cargo}')
+ativo = vendas[4]
+print(f'Usuário ativo: {ativo}')
