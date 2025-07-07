31b1e3b5d7da7ce909fe15bb3057763e1c63633f
Michele <michele.nascf@gmail.com>
2025-07-07 08:15:30 -0300

    Adiciona método set para remover duplicidade.

--- fundamentos_iniciais/dicionario.py
+++ fundamentos_iniciais/dicionario.py
@@ -59,3 +59,7 @@ print(dicionario_ab)
 
 for i in range(1, 10, 2):
     print(i)
+
+duplicidade = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5]
+remove_duplicados = set(duplicidade)
+print(remove_duplicados)
