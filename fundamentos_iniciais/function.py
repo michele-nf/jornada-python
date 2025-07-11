0785a85dfbe1586fa0453cd356781200ebc9783b
Michele <michele.nascf@gmail.com>
2025-07-11 09:49:16 -0300

    Adiciona função com mais de um parâmetro e return.

--- fundamentos_iniciais/function.py
+++ fundamentos_iniciais/function.py
@@ -7,4 +7,23 @@ def cadastrar_produto():
 produto = cadastrar_produto()
 print(f'Produto cadastrado: {produto}')
 
+def somar_numeros(num1, num2, num3):
+    return num1 + num2 + num3
 
+soma = somar_numeros(10, 20, 30)
+print(f'Soma dos números: {soma}')
+
+produtos = ['beb46275','TFA23962','TFA64715','TFA69555','TFA56743','BSA45510','TFA44968','CAR75448','CAR23596','CAR13490','BEB21365','BEB31623','BSA62419','BEB73344','TFA20079','BEB80694','BSA11769','BEB19495','TFA14792','TFA78043','BSA33484','BEB97471','BEB62362','TFA27311','TFA17715','BEB85146','BEB48898','BEB79496','CAR38417','TFA19947','TFA58799','CAR94811','BSA59251','BEB15385','BEB24213','BEB56262','BSA96915','CAR53454','BEB75073']
+
+def eh_da_categoria(bebida, cod_categoria):
+    bebida = bebida.upper()
+    if cod_categoria in bebida:
+        return True
+    else:
+        return False
+    
+for produto in produtos:
+    if eh_da_categoria(produto, 'BEB'):
+        print(f'{produto} é uma bebida alcoólica.')
+    elif eh_da_categoria(produto, 'BSA'):
+        print(f'{produto} não é uma bebida alcoólica.')
