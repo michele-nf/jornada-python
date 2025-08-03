a948da2857598e2f3eab567620c96ecd37706c1f
Michele <michele.nascf@gmail.com>
2025-08-03 16:58:22 -0300

    Adiciona condições em list comprehension.

--- fundamentos_iniciais/list_comprehension.py
+++ fundamentos_iniciais/list_comprehension.py
@@ -27,3 +27,9 @@ produtos = ['vinho', 'cafeiteira', 'microondas', 'iphone']
 
 produtos_acima_meta = [produto for i, produto in enumerate(produtos) if vendas_produtos[i] > meta]
 print(produtos_acima_meta)
+
+vendedores_dic = {'Maria': 1200, 'José': 300, 'Antônio': 800, 'João': 1500, 'Francisco': 1900, 'Ana': 2750, 'Luiz': 400, 'Paulo': 20, 'Carlos': 23, 'Manoel': 70, 'Pedro': 90, 'Francisca': 80, 'Marcos': 1100, 'Raimundo': 999, 'Sebastião': 900, 'Antônia': 880, 'Marcelo': 870, 'Jorge': 50, 'Márcia': 1111, 'Geraldo': 120, 'Adriana': 300, 'Sandra': 450, 'Luis': 800}
+meta = 1000
+
+bonus_vendedores = [vendedores_dic[item] * 0.1 if vendedores_dic[item] > meta else 0 for item in vendedores_dic]
+print(bonus_vendedores)
