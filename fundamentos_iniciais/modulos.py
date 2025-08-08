212961f370179557b8d10d5f9f5505a8a4751f14
Michele <michele.nascf@gmail.com>
2025-08-08 06:26:47 -0300

    Adiciona módulo time, utilizando algumas funções.

--- fundamentos_iniciais/modulos.py
+++ fundamentos_iniciais/modulos.py
@@ -1,3 +1,27 @@
 import webbrowser
+import time
 
 webbrowser.open("https://docs.python.org/3/tutorial/modules.html")
+
+segundos_hoje = time.time()
+print(f"Segundos desde a época: {segundos_hoje}")
+
+data_hoje = time.ctime()
+print(f"Data e hora atual: {data_hoje}")
+
+print("Começando")
+time.sleep(5)
+print("Rodou 5 segundos após")
+
+hora_geral = time.gmtime()
+hora_local = time.localtime()
+print(hora_geral)
+print(hora_local)
+
+dia = hora_local.tm_mday
+mes = hora_local.tm_mon
+ano = hora_local.tm_year
+dia_da_semana = hora_local.tm_wday
+
+print("Data: {}/{}/{}".format(dia, mes, ano))
+print(f"Data: {dia}/{mes}/{ano}")
