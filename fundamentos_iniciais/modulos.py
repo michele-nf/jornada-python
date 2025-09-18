b311063b178083175623a1f2fd92254f2a6afca1
Michele <michele.nascf@gmail.com>
2025-09-18 08:20:22 -0300

    Adiciona lógica para exibição de gráfico.

--- fundamentos_iniciais/modulos.py
+++ fundamentos_iniciais/modulos.py
@@ -1,6 +1,7 @@
 import webbrowser
 import time
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 webbrowser.open("https://docs.python.org/3/tutorial/modules.html")
 
@@ -34,7 +35,6 @@ hora = agora.time()
 print(data)
 print(hora)
 
-#O usuário fornece a data de nascimento no formato dd/mm/aaaa. Crie um script python que calcula a idade do usuário.
 data_nascimento = input("Digite sua data de nascimento (dd/mm/aaaa): ")
 data_nascimento = datetime.strptime(data_nascimento, "%d/%m/%Y")
 data_atual = datetime.now()
@@ -44,3 +44,14 @@ if (data_atual.month, data_atual.day) < (data_nascimento.month, data_nascimento.
     idade -= 1
 
 print(f"Sua idade é: {idade} anos")
+
+
+vendas = [1500, 1800, 1200, 2200, 2500, 3000, 2800, 3200, 4000, 4500, 5000, 6000]
+meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+
+plt.plot(meses, vendas)
+plt.title("Vendas Mensais")
+plt.xlabel("Meses")
+plt.ylabel("Vendas (R$)")
+plt.axis(0, 11, 0, max(vendas) + 500)
+plt.show()
